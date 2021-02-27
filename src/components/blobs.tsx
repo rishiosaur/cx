4a2007978e1e96df3e2f2d3276f0f5fc8d8a9469
Rishi Kothari <itsrishikothari@gmail.com>
2021-02-27 13:56:47 -0500

    fix eslint's blathering

--- src/components/blobs.tsx
+++ src/components/blobs.tsx
@@ -13,34 +13,21 @@ export const Blob: React.FC<{
 	startx: string
 	starty: string
 	size: string
-}> = (props) => {
-	const isSmall = useMedia('(max-width: 1024px)')
-	// const { scrollYProgress } = useViewportScroll();
-	// const [rot, set] = useState(0);
-	// const rotate = useTransform(scrollYProgress, x => 360*(1-(x/2))).onChange(z => set(z));
-	// console.log('z', props.startx);
-	// useEffect(() => {
-	// 	console.log(Color(props.colors[0]).rotate(rot).hex())
-	// }, [props.colors, rot])
-	return (
+}> = ({  colors, startx, starty, size  }) => (
 		<motion.div
 			style={{
 				borderRadius: '30% 50% 20% 40%',
-				backgroundColor: props.colors[0],
-				background: `radial-gradient(circle, ${props.colors[0]} 0%,  ${props.colors[1]} 100%);`,
-				height: props.size,
-				width: props.size,
+				backgroundColor: colors[0],
+				background: `radial-gradient(circle, ${colors[0]} 0%,  ${colors[1]} 100%);`,
+				height: size,
+				width: size,
 				animation: `transform 40s ease-in-out infinite both alternate, movement${sample(
 					['1', '2', '3', '4']
 				)} 20s ease-in-out infinite both`,
 				opacity: 0.7,
 				position: 'absolute',
-				left: props.startx,
-				top: props.starty,
-			}}
-			onScroll={() => {
-				console.log('scrolled')
+				left: startx,
+				top: starty,
 			}}
 		/>
 	)
-}
