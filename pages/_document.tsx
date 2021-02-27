307da5d68ab912697d3b82fcb4609553f34f0312
Rishi Kothari <itsrishikothari@gmail.com>
2021-02-27 13:54:23 -0500

    document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,13 +9,48 @@ class MyDocument extends Document {
 
 	render() {
 		return (
-			<Html>
-				<Head />
-				<body>
-					<Main />
-					<NextScript />
-				</body>
-			</Html>
+			<>
+				<Html lang="en">
+					<head>
+						<link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+						<meta
+							name="viewport"
+							content="width=device-width, initial-scale=1.0"
+						/>
+
+						<title>Rishi Kothari | Software Engineer, Developer</title>
+						<link rel="shortcut icon" href="https://i.ibb.co/TPKjZbS/1604320263101.jpg" />
+						<link rel="preload"/>
+						<meta name="title" content="Rishi Kothari | Software Engineer, Developer"/>
+							<meta name="description" content="I'm Rishi Kothari, a 15-year-old developer that's excited about backend technologies with GraphQL, and the intersection of CS Theory with social good."/>
+
+
+								<meta property="og:type" content="website"/>
+									<meta property="og:url" content="https://rishi.cx/"/>
+										<meta property="og:title" content="Rishi Kothari | Software Engineer, Developer"/>
+											<meta property="og:description" content="I'm Rishi Kothari, a 15-year-old developer that's excited about backend technologies with GraphQL, and the intersection of CS Theory with social good."/>
+												<meta property="og:image" content="https://i.ibb.co/TPKjZbS/1604320263101.jpg"/>
+
+
+													<meta property="twitter:card" content="summary_large_image"/>
+														<meta property="twitter:url" content="https://rishi.cx/"/>
+															<meta property="twitter:title" content="Rishi Kothari | Software Engineer, Developer"/>
+																<meta property="twitter:description" content="I'm Rishi Kothari, a 15-year-old developer that's excited about backend technologies with GraphQL, and the intersection of CS Theory with social good."/>
+																	<meta property="twitter:image" content="https://i.ibb.co/TPKjZbS/1604320263101.jpg"/>
+					</head>
+					<Head />
+
+					<body>
+						<Main />
+						<NextScript />
+						<script src="https://unpkg.com/aos@next/dist/aos.js" />
+						<script>
+							AOS.init();
+						</script>
+					</body>
+
+				</Html>
+			</>
 		)
 	}
 }
