f24074cff0a99aef84e420d8271e983d86a66aba
Rishi Kothari <itsrishikothari@gmail.com>
2021-02-27 13:55:01 -0500

    format document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,31 +12,59 @@ class MyDocument extends Document {
 			<>
 				<Html lang="en">
 					<head>
-						<link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+						<link
+							rel="stylesheet"
+							href="https://unpkg.com/aos@next/dist/aos.css"
+						/>
 						<meta
 							name="viewport"
 							content="width=device-width, initial-scale=1.0"
 						/>
 
 						<title>Rishi Kothari | Software Engineer, Developer</title>
-						<link rel="shortcut icon" href="https://i.ibb.co/TPKjZbS/1604320263101.jpg" />
-						<link rel="preload"/>
-						<meta name="title" content="Rishi Kothari | Software Engineer, Developer"/>
-							<meta name="description" content="I'm Rishi Kothari, a 15-year-old developer that's excited about backend technologies with GraphQL, and the intersection of CS Theory with social good."/>
+						<link
+							rel="shortcut icon"
+							href="https://i.ibb.co/TPKjZbS/1604320263101.jpg"
+						/>
+						<link rel="preload" />
+						<meta
+							name="title"
+							content="Rishi Kothari | Software Engineer, Developer"
+						/>
+						<meta
+							name="description"
+							content="I'm Rishi Kothari, a 15-year-old developer that's excited about backend technologies with GraphQL, and the intersection of CS Theory with social good."
+						/>
 
+						<meta property="og:type" content="website" />
+						<meta property="og:url" content="https://rishi.cx/" />
+						<meta
+							property="og:title"
+							content="Rishi Kothari | Software Engineer, Developer"
+						/>
+						<meta
+							property="og:description"
+							content="I'm Rishi Kothari, a 15-year-old developer that's excited about backend technologies with GraphQL, and the intersection of CS Theory with social good."
+						/>
+						<meta
+							property="og:image"
+							content="https://i.ibb.co/TPKjZbS/1604320263101.jpg"
+						/>
 
-								<meta property="og:type" content="website"/>
-									<meta property="og:url" content="https://rishi.cx/"/>
-										<meta property="og:title" content="Rishi Kothari | Software Engineer, Developer"/>
-											<meta property="og:description" content="I'm Rishi Kothari, a 15-year-old developer that's excited about backend technologies with GraphQL, and the intersection of CS Theory with social good."/>
-												<meta property="og:image" content="https://i.ibb.co/TPKjZbS/1604320263101.jpg"/>
-
-
-													<meta property="twitter:card" content="summary_large_image"/>
-														<meta property="twitter:url" content="https://rishi.cx/"/>
-															<meta property="twitter:title" content="Rishi Kothari | Software Engineer, Developer"/>
-																<meta property="twitter:description" content="I'm Rishi Kothari, a 15-year-old developer that's excited about backend technologies with GraphQL, and the intersection of CS Theory with social good."/>
-																	<meta property="twitter:image" content="https://i.ibb.co/TPKjZbS/1604320263101.jpg"/>
+						<meta property="twitter:card" content="summary_large_image" />
+						<meta property="twitter:url" content="https://rishi.cx/" />
+						<meta
+							property="twitter:title"
+							content="Rishi Kothari | Software Engineer, Developer"
+						/>
+						<meta
+							property="twitter:description"
+							content="I'm Rishi Kothari, a 15-year-old developer that's excited about backend technologies with GraphQL, and the intersection of CS Theory with social good."
+						/>
+						<meta
+							property="twitter:image"
+							content="https://i.ibb.co/TPKjZbS/1604320263101.jpg"
+						/>
 					</head>
 					<Head />
 
@@ -44,11 +72,8 @@ class MyDocument extends Document {
 						<Main />
 						<NextScript />
 						<script src="https://unpkg.com/aos@next/dist/aos.js" />
-						<script>
-							AOS.init();
-						</script>
+						<script>AOS.init();</script>
 					</body>
-
 				</Html>
 			</>
 		)
