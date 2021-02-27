577b690fa11a7b7cd5042d375d0eab8530e53651
Rishi Kothari <itsrishikothari@gmail.com>
2021-02-27 13:54:59 -0500

    format app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,34 +1,40 @@
 import React, { useEffect, useState } from 'react'
-import { AnimateSharedLayout, motion, useMotionValue, useSpring, useViewportScroll } from 'framer-motion'
+import {
+	AnimateSharedLayout,
+	motion,
+	useMotionValue,
+	useSpring,
+	useViewportScroll,
+} from 'framer-motion'
 import './app.scss'
 import theme from '../src/theme'
 
 const App = ({ Component, pageProps }) => {
-	const { scrollY  } = useViewportScroll()
+	const { scrollY } = useViewportScroll()
 
-	return(
+	return (
+		<>
+			<AnimateSharedLayout>
+				<Component {...pageProps} />
+			</AnimateSharedLayout>
+			<style jsx global>
+				{`
+					body {
+						background-color: ${theme.colors.background};
+						color: ${theme.colors.color};
+					}
+				`}
+			</style>
 
-	<>
-		<AnimateSharedLayout>
-			<Component {...pageProps} />
-		</AnimateSharedLayout>
-		<style jsx global>
-			{`
-        body {
-          background-color: ${theme.colors.background};
-          color: ${theme.colors.color};
-          }  
-          
-          `}</style>
-
-		{/* <motion.div
+			{/* <motion.div
 				className="cursor"
 				style={{
 					translateX: cursorX,
 					translateY: cursorY,
 				}}
 			/> */}
-	</>
-)}
+		</>
+	)
+}
 
 export default App
