a6ebfd30abaa2f404ffa0f2ef8c27749c71bd22f
Rishi Kothari <itsrishikothari@gmail.com>
2021-02-28 13:21:57 -0500

    update util for cms func

--- src/util.ts
+++ src/util.ts
@@ -1,2 +1,10 @@
+import request from 'graphql-request'
+
 export const sample = <T>(items: T[]) =>
 	items[Math.floor(Math.random() * items.length)]
+
+export const fetch = (query: string) =>
+	request(
+		'https://api-us-east-1.graphcms.com/v2/ckjq7p0mau50s01z1aio1b4ia/master',
+		query
+	)
